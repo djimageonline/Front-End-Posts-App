f3b8fc62eb587fb5dbd3825523a552f010b494e3
Johnny Proano <djimageonline@gmail.com>
2022-12-11 12:01:50 -0500

    impleentd Modal, called posts in modal, adjusted button more info, to open modal and post and a close button

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,9 +2,12 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { PostsNew } from "./PostsNew";
 import { PostsIndex } from "./PostsIndex";
+import { Modal } from "./Modal";
 
 export function Home() {
   const [posts, setPosts] = useState([]);
+  const [isBlogsShowModalVisible, setIsBlogsShowModalVisible] = useState(false);
+  const [currentBlog, setCurrentBlog] = useState({});
 
   const handleIndexPosts = () => {
     axios.get("http://localhost:3000/posts.json").then((response) => {
@@ -13,13 +16,26 @@ export function Home() {
     });
   };
 
+  const handleShowBlogs = (post) => {
+    setIsBlogsShowModalVisible(true);
+    setCurrentBlog(post);
+  };
+
+  const handleHideBlogs = () => {
+    setIsBlogsShowModalVisible(false);
+  };
+
   useEffect(handleIndexPosts, []);
 
   return (
     <div>
       <PostsNew />
-      <PostsIndex posts={posts} />
-      <button onClick={handleIndexPosts}> Load Posts</button>
+      <PostsIndex posts={posts} onSelectBlog={handleShowBlogs} />
+      {/* <button onClick={handleIndexPosts}> Load Posts</button> */}
+      <Modal show={isBlogsShowModalVisible} onClose={handleHideBlogs}>
+        <h2>{currentBlog.title}</h2>
+        <p>Post: {currentBlog.body}</p>
+      </Modal>
     </div>
   );
 }
--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,14 +1,14 @@
 export function PostsIndex(props) {
-  console.log(props);
+  console.log(props.onSelectBlog);
   return (
     <div id="posts-index">
       <h1>All posts</h1>
       {props.posts.map((post) => (
         <div key={post.id} className="posts">
           <h2>{post.title}</h2>
-          <img src={post.image_url} alt="" />
-          <p>Chef: {post.body}</p>
-          <button>More Info</button>
+          <img src={post.image} alt="" />
+          <p>Post: {post.body}</p>
+          <button onClick={() => props.onSelectBlog(post)}>More Info</button>
         </div>
       ))}
     </div>
